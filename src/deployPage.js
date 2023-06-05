f2f4ce062aa89364e3f1333806e1d361638f8f0c
Remi03 <t108590003@ntut.org.tw>
2023-06-06 05:48:27 +0800

    try

--- src/deployPage.js
+++ src/deployPage.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { deploy } from './ethereum/deploy';
 import { web3 } from './ethereum/web3';
 import axios from 'axios';
 
 const DeployPage = () => {
-  // const [setContractAddress] = useState('');
+  const [contractAddress, setContractAddress] = useState('');
   const [getParams] = useSearchParams();
   const buyerID = getParams.getAll('buyerID');
   const buyerAddress = getParams.getAll('buyerAddress');
@@ -18,29 +18,29 @@ const DeployPage = () => {
     e.preventDefault(); 
 
     const address = await deploy(sellerWalletAddress, totalPrice);
-    // setContractAddress(address);
+    setContractAddress(address);
 
-    if (address) {
-      const data = {
-        buyerID: buyerID[0],
-        buyerAddress: buyerAddress[0],
-        smartContractAddress: address,
-        storeID: storeID[0],
-        serverID: serverID[0]
-      }
-      try {
-          const msg = await axios.post('https://220.134.59.172:7226/OrderQuery/OrderSave', data, {
-            headers: {
-              "Access-Control-Allow-Origin": "*"
-            }
-          });
-          console.log(msg);
-          // 在這裡可以添加一些處理成功傳送的邏輯或響應給用戶的訊息
-      } catch (error) {
-        console.error('無法傳送訂單資料到後端：', error);
-        // 在這裡可以添加一些處理錯誤的邏輯或響應給用戶的訊息
-      }
-    }
+    // if (address) {
+    // const data = {
+    //   buyerID: buyerID[0],
+    //   buyerAddress: buyerAddress[0],
+    //   smartContractAddress: address,
+    //   storeID: storeID[0],
+    //   serverID: serverID[0]
+    // }
+      // try {
+      //     const msg = await axios.post('https://220.134.59.172:7226/OrderQuery/OrderSave', data, {
+      //       headers: {
+      //         "Access-Control-Allow-Origin": "*"
+      //       }
+      //     });
+      //     console.log(msg);
+      //     // 在這裡可以添加一些處理成功傳送的邏輯或響應給用戶的訊息
+      // } catch (error) {
+      //   console.error('無法傳送訂單資料到後端：', error);
+      //   // 在這裡可以添加一些處理錯誤的邏輯或響應給用戶的訊息
+      // }
+    // }
 
   };
 
@@ -52,16 +52,22 @@ const DeployPage = () => {
           Total Price : {totalPrice} ETH
         </label>
         <p><button type="submit">Deploy Contract</button></p>
+        <h3>Url: https://220.134.59.172:7226/OrderQuery/OrderSave</h3>
+        <p>buyerID: {buyerID[0]}</p>
+        <p>buyerAddress: {buyerAddress[0]}</p>
+        {/* <p>smartContractAddress: {address}</p> */}
+        <p>storeID: {storeID[0]}</p>
+        <p>serverID: {serverID[0]}</p>
       </form>
 
       <p>seller address: {totalPrice}</p>
 
-      {/* {contractAddress && (
+      {contractAddress && (
         <div>
           <p>Contract deployed at address: {contractAddress}</p>
-          <button onClick={() => navigate(`/contract/${contractAddress}`)}>Go to Contract Page</button>
+          {/* <button onClick={() => navigate(`/contract/${contractAddress}`)}>Go to Contract Page</button> */}
         </div>
-      )} */}
+      )}
     </div>
   );
 };
